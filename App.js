e0b850f5a936393cab295886bddce2f924fcf16c
Sakina <skn11399@gmail.com>
2020-10-10 17:51:34 +0900

    App.jsの不要な記述を削除

--- App.js
+++ App.js
@@ -4,27 +4,30 @@ import { StyleSheet, View } from 'react-native';
 // import BodyText from './src/elements/BodyText';
 import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import SignupScreen from './src/screens/SignupScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+// import SignupScreen from './src/screens/SignupScreen';
  
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import firebase from 'firebase';
 
-require("firebase/firestore");
+// require("firebase/firestore");
 
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
 
 
 const App = createStackNavigator({
-  Login:      { screen: LoginScreen },
-  MemoCreate: { screen: MemoCreateScreen},
-  Signup:     { screen: SignupScreen },
-  Home:       { screen: MemoListScreen },
-  MemoDetail: { screen: MemoDetailScreen },
-  MemoEdit:   { screen: MemoEditScreen },
+  // Login:      { screen: LoginScreen },
+  Home: { screen: HomeScreen },
+  // MemoCreate: { screen: MemoCreateScreen},
+  // Signup:     { screen: SignupScreen },
+  // Home:       { screen: MemoListScreen },
+  // MemoDetail: { screen: MemoDetailScreen },
+  // MemoEdit:   { screen: MemoEditScreen },
 }, {
   defaultNavigationOptions: {
     headerBackTitle: "　",
